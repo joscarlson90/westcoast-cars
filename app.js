58a9567002c9e09ad6bc5c0bb4b1c8244489464b
Josefine Carlson <josefinecarlson90@gmail.com>
2026-01-19 13:51:35 +0100

    added basic code for loading vehicles

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const initApp = () => {
-    console.log("initApp is running!");
+  console.log("initApp is running!");
+  loadVehicles();
+};
+
+const loadVehicles = () => {
+  console.log("Loading vehicles");
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
