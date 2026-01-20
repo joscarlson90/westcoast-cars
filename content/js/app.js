6e25898f638bff25da6a1d3ad55834d424f083f0
Josefine Carlson <josefinecarlson90@gmail.com>
2026-01-20 17:51:38 +0100

    changed everything for fun

--- content/js/app.js
+++ content/js/app.js
@@ -10,8 +10,22 @@ const loadVehicles = async () => {
     const result = await vehicles.json();
     console.log(result);
 
-     result.forEach((vehicle) => {
-      console.log(vehicle.id, vehicle.manufacturer, vehicle.model, vehicle.modelYear, vehicle.mileAge);
+    const carContainer = document.querySelector(".carContainer");
+    const cars = document.querySelector(".cars");
+
+    result.forEach((vehicle) => {
+      console.log(
+        vehicle.id,
+        vehicle.manufacturer,
+        vehicle.model,
+        vehicle.modelYear,
+        vehicle.mileAge,
+      );
+
+      const carDiv = document.createElement("div");
+      carDiv.innerHTML = `<p><strong>${vehicle.manufacturer}</strong></p><p>Modell: ${vehicle.model}</p><p>År: ${vehicle.modelYear}</p><p>Mil: ${vehicle.mileAge}</p>`;
+      carContainer.appendChild(carDiv);
+      carDiv.classList.add("carDiv");
     });
     return;
   }
@@ -19,3 +33,11 @@ const loadVehicles = async () => {
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+/* 1. utöka json filen med 
+   modelYear,
+   mileAge
+2. uppdatera app.js
+   skriv ut i konsolfönstret alla egenskaper
+3. skapa en visualisering i index.html som visar samma inmatning
+4. pusha till github */
