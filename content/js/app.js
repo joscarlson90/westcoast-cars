c877694fffdd0b0e7de42f1b96d50128fd97049e
Josefine Carlson <josefinecarlson90@gmail.com>
2026-01-22 20:12:30 +0100

    finished the task

--- content/js/app.js
+++ content/js/app.js
@@ -1,48 +1,86 @@
 const initApp = () => {
   console.log("initApp is running!");
-  loadVehicles();
+  
 };
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.querySelector(".carContainer")) {
+    loadVehicles();
+  }
+
+  if (document.querySelector(".vehicleInfo")) {
+    loadVehicleInfo();
+  }
+});
 
 const loadVehicles = async () => {
-  console.log("Loading vehicles");
-  const vehicles = await fetch("http://localhost:3000/vehicles");
-  if (vehicles.ok) {
-    const result = await vehicles.json();
-    console.log(result);
-    
-    const carContainer = document.querySelector(".carContainer");
-    
-    result.forEach((vehicle) => {
-      console.log(
-        vehicle.id,
-        vehicle.manufacturer,
-        vehicle.model,
-        vehicle.modelYear,
-        vehicle.mileAge,
-        vehicle.value
-      );
-      
-      const carDiv = document.createElement("div");
-      carDiv.innerHTML = `<p><strong>${vehicle.manufacturer}</strong></p><p>Model: ${vehicle.model}</p><p>Year: ${vehicle.modelYear}</p><p>Mile Age: ${vehicle.mileAge}</p><p>Value: ${vehicle.value}</p>`;
-      carContainer.appendChild(carDiv);
-      carDiv.classList.add("carDiv");
+  const response = await fetch("http://localhost:3000/vehicles");
+  const vehicles = await response.json();
+
+  const container = document.querySelector(".carContainer");
+
+  vehicles.forEach((vehicle) => {
+    const carDiv = document.createElement("div");
+    carDiv.innerHTML = `<p><strong>${vehicle.manufacturer}</strong>`;
+    carDiv.classList.add("carDiv");
+
+    carDiv.addEventListener("click", () => {
+      window.location.href = "vehicle-detail.html";
     });
-    return;
-  }
-  console.log("Hoppsan, det gick lite fel..");
+    container.appendChild(carDiv);
+  });
 };
 
+const loadVehicleInfo = async () => {
+  const response = await fetch("http://localhost:3000/vehicles");
+  const vehicles = await response.json();
+
+  const container = document.querySelector(".vehicleInfo");
+
+  vehicles.forEach((vehicle) => {
+    const carDiv = document.createElement("div");
+    carDiv.innerHTML = `<p><strong>${vehicle.manufacturer}</strong>:<li>${vehicle.model}</li><li>${vehicle.modelYear}</li><li>${vehicle.mileAge}</li><li>${vehicle.value}</li>`;
+    carDiv.classList.add("carDiv");
+    carDiv.addEventListener("click", () => {
+      window.location.href = "vehicle-detail.html";
+  });
+  container.appendChild(carDiv);
+  });
+};
+
+// // const initApp = () => {
+// //   console.log("initApp is running!");
+// //   loadVehicles();
+// // };
+
+// // const loadVehicles = async () => {
+// //   console.log("Loading vehicles");
+// //   const vehicles = await fetch("http://localhost:3000/vehicles");
+// //   if (vehicles.ok) {
+// //     const result = await vehicles.json();
+// //     console.log(result);
+
+// //     const carContainer = document.querySelector(".carContainer");
+
+// //     result.forEach((vehicle) => {
+// //       console.log(
+// //         vehicle.id,
+// //         vehicle.manufacturer,
+// //         vehicle.model,
+// //         vehicle.modelYear,
+// //         vehicle.mileAge,
+// //         vehicle.value
+// //       );
+
+// //       const carDiv = document.createElement("div");
+// //       carDiv.innerHTML = `<p><strong>${vehicle.manufacturer}</strong></p><p>Model: ${vehicle.model}</p><p>Year: ${vehicle.modelYear}</p><p>Mile Age: ${vehicle.mileAge}</p><p>Value: ${vehicle.value}</p>`;
+// //       carContainer.appendChild(carDiv);
+// //       carDiv.classList.add("carDiv");
+// //     });
+// //     return;
+// //   }
+// //   console.log("Hoppsan, det gick lite fel..");
+// };
+
 document.addEventListener("DOMContentLoaded", initApp);
-
-/* 1. utöka json filen med 
-   modelYear,
-   mileAge
-2. uppdatera app.js
-   skriv ut i konsolfönstret alla egenskaper
-3. skapa en visualisering i index.html som visar samma inmatning
-4. pusha till github 
-
-1. Klicka på bilmärket, tas vidare till en annan html-sida. Ex. vehicle-detail.html 
-Alt - baka in en a-tag i h4 */
-
-
